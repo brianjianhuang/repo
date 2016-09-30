d80953b7da0d122c028f98ea2e407ada082609b9
brianjianhuang <brianjianhuang@users.noreply.github.com>
2016-09-30 17:25:55 -0400

    Update UserDaoTest.java

--- src/main/java/es/util/UserDaoTest.java
+++ src/main/java/es/util/UserDaoTest.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import es.util.User.City;
+
 public class UserDaoTest {
 
 	public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException,
@@ -16,25 +18,33 @@ public class UserDaoTest {
 
 		
 		//Create a new User
-		User.Address address = new User.Address("2 apple court", "Jersey City");
+		User.City city = new User.City("Jersey City", "12345");
+		User.Address address = new User.Address("2 apple court");
+		address.setCity(city);
 		String id = "" + System.currentTimeMillis();
 		User newUser = new User(id, "Mary " + id.substring(1, 10), "coder", address);
-		userDao.save(newUser);
+
 		System.out.println("Store user: " + newUser);
-		
+
+		 
+		userDao.save(newUser);
+
 		//find by id
 		User user = userDao.findUserById(id);
 		System.out.println("user: " + user);
 		
 		//Convert es User to a partial UserView
 		ObjectMapper objectMapper = new ObjectMapper();
-		UserView userView = objectMapper.convertValue(user, UserView.class);
-		System.out.println("userView: " + userView);
+		//UserView userView = objectMapper.convertValue(user, UserView.class);
+		//System.out.println("userView: " + userView);
 		
-		List<UserView> userViews = userDao.findUserViewByName("Mary");
-		System.out.println("Found user name Mary: " + userViews.size());
+		ExtendableUserView extendableUserView = objectMapper.convertValue(user, ExtendableUserView.class);
+		System.out.println("extendableUserView: " + extendableUserView);
+		
+		//List<UserView> userViews = userDao.findUserViewByName("Mary");
+		//System.out.println("Found user name Mary: " + userViews.size());
 
-		 
+	 
 	}
 
 }
